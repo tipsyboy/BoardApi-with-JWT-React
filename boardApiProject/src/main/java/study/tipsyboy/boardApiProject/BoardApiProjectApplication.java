97b8fd5e91e2dad9e0bf9dc22b09d828a9dc8f9e
TipsyBoy <gudah13@naver.com>
2023-08-15 13:48:23 +0900

    Initial Commmit

---
+++ boardApiProject/src/main/java/study/tipsyboy/boardApiProject/BoardApiProjectApplication.java
@@ -0,0 +1,13 @@
+package study.tipsyboy.boardApiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApiProjectApplication.class, args);
+	}
+
+}
---
+++ boardApiProject/src/test/java/study/tipsyboy/boardApiProject/BoardApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package study.tipsyboy.boardApiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
